6f053cd3fe27a7136852d15aa61998ac941546bd
Charlie Russ <charlieruss2128@gmail.com>
2015-12-13 16:53:04 -0800

    Now passing lint.

--- challenges/math_challenge.go
+++ challenges/math_challenge.go
@@ -1,47 +1,52 @@
 package challenges
 
 import (
-    "math/rand"
+	"math/rand"
 )
 
-type math_challenge struct {
-    seed int64
-    seeded bool
+// MathChallenge wrapper struct.
+type MathChallenge struct {
+	seed   int64
+	seeded bool
 }
 
+// MathProblem wrapper struct.
 type MathProblem struct {
-    Value1 int64
-    Value2 int64
-    Symbol string
+	Value1 int64
+	Value2 int64
+	Symbol string
 }
 
-func (mc *math_challenge) GetProblem() MathProblem {
-    return MathProblem{
-        Value1: int64(mc.getRandomInt()),
-        Value2: int64(mc.getRandomInt()),
-        Symbol: "+",
-    }
+// GetProblem retrieves a MathProblem struct with random values initialized.
+func (mc *MathChallenge) GetProblem() MathProblem {
+	return MathProblem{
+		Value1: int64(mc.getRandomInt()),
+		Value2: int64(mc.getRandomInt()),
+		Symbol: "+",
+	}
 }
 
-func (mc *math_challenge) getRandomInt() int {
-    if !mc.seeded {
-        rand.Seed(mc.seed)
-        mc.seeded = true
-    }
-    return rand.Intn(100)
+func (mc *MathChallenge) getRandomInt() int {
+	if !mc.seeded {
+		rand.Seed(mc.seed)
+		mc.seeded = true
+	}
+	return rand.Intn(100)
 }
 
-func NewMathChallenge(seed int64) math_challenge {
-    return math_challenge{seed: seed, seeded: false}
+// NewMathChallenge wrapper for creating a math challenge.
+func NewMathChallenge(seed int64) MathChallenge {
+	return MathChallenge{seed: seed, seeded: false}
 }
 
+// Solution retrieves the solution for a MathProblem.
 func (mp *MathProblem) Solution() int64 {
-    switch mp.Symbol {
-    case "+":
-        return mp.Value1 + mp.Value2
-    case "-":
-        return mp.Value1 - mp.Value2
-    default:
-        panic("unrecognized Symbol")
-    }
+	switch mp.Symbol {
+	case "+":
+		return mp.Value1 + mp.Value2
+	case "-":
+		return mp.Value1 - mp.Value2
+	default:
+		panic("unrecognized Symbol")
+	}
 }
--- main.go
+++ main.go
@@ -1,67 +1,67 @@
 package main
 
 import (
-    "github.com/MilkyC/Brainium/challenges"
-    "fmt"
-    "time"
+	"fmt"
+	"github.com/MilkyC/Brainium/challenges"
+	"time"
 )
 
 type countdown struct {
-    count int
-    ticker *time.Ticker
+	count  int
+	ticker *time.Ticker
 }
 
 func main() {
-    countdowner := countdown{
-        count: 0,
-        ticker: time.NewTicker(time.Millisecond * 1000),
-    }
-    seed := int64(time.Now().Unix())
-    mc := challenges.NewMathChallenge(seed)
+	countdowner := countdown{
+		count:  0,
+		ticker: time.NewTicker(time.Millisecond * 1000),
+	}
+	seed := int64(time.Now().Unix())
+	mc := challenges.NewMathChallenge(seed)
 
-    for true {
-        user_done := make(chan bool, 1)
-        countdown_done := make(chan bool, 1)
-        go func(countdown_done chan<- bool) {
-            for range countdowner.ticker.C {
-                countdowner.count += 1
-                fmt.Printf("%d seconds\n", countdowner.count)
-                if countdowner.count > 10 {
-                    countdown_done <- true
-                }
-            }
-        }(countdown_done)
+	for true {
+		userDone := make(chan bool, 1)
+		countdownDone := make(chan bool, 1)
+		go func(countdownDone chan<- bool) {
+			for range countdowner.ticker.C {
+				countdowner.count++
+				fmt.Printf("%d seconds\n", countdowner.count)
+				if countdowner.count > 10 {
+					countdownDone <- true
+				}
+			}
+		}(countdownDone)
 
-        go func(user_done chan<- bool) {
-            mp := mc.GetProblem()
-            fmt.Printf("%d %s %d\n", mp.Value1, mp.Symbol, mp.Value2)
-            var input int64
-            _, err := fmt.Scanln(&input)
-            if err != nil {
-                 fmt.Println("Error: ", err)
-            }
-            fmt.Println("Input: ", input)
-            solution := mp.Solution()
-            if input == solution{
-                fmt.Println("Correct!")
-            } else {
-                fmt.Println("Wrong! Expected Answer:", solution)
-            }
-            user_done <- true
-        }(user_done)
+		go func(userDone chan<- bool) {
+			mp := mc.GetProblem()
+			fmt.Printf("%d %s %d\n", mp.Value1, mp.Symbol, mp.Value2)
+			var input int64
+			_, err := fmt.Scanln(&input)
+			if err != nil {
+				fmt.Println("Error: ", err)
+			}
+			fmt.Println("Input: ", input)
+			solution := mp.Solution()
+			if input == solution {
+				fmt.Println("Correct!")
+			} else {
+				fmt.Println("Wrong! Expected Answer:", solution)
+			}
+			userDone <- true
+		}(userDone)
 
-        finished := false
+		finished := false
 
-        for !finished {
-            select {
-            case <-countdown_done:
-                fmt.Println("countdown finished first")
-                finished = true
-            case <-user_done:
-                fmt.Println("user finished first")
-                finished = true
-            }
-        }
-        countdowner.count = 0
-    }
+		for !finished {
+			select {
+			case <-countdownDone:
+				fmt.Println("countdown finished first")
+				finished = true
+			case <-userDone:
+				fmt.Println("user finished first")
+				finished = true
+			}
+		}
+		countdowner.count = 0
+	}
 }
